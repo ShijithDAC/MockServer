e91dfd926d191c37ce15f3b9183b676fd053d389
ShijithDAC <shijith@digitalapicraft.com>
2018-04-06 00:19:36 +0530

    adding eslint and jshint for standards

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,34 @@
 const express = require('express');
+/*eslint-disable */
 const router = express.Router();
+/*eslint-enable */
 
 /* GET home page. */
 router.get('/',(req,res,next)=> {
 
-  res.json({"message":"Hello, Welcome to MOCK Server"});
+    res.json({"message": "Hello, Welcome to MOCK Server"});
 
 });
 
 router.post("/mock",(req,res,next)=>{
     
-  const user = req.body.username;
-  const pass = req.body.password;
+    const user = req.body.username;
+    const pass = req.body.password;
 
-  if (user === "MYusername" && pass === "MYpassword")
-  {
+    if (user === "MYusername" && pass === "MYpassword")
+    {
 
-      console.log("Success");
-      res.status(200);
-      res.json({"message": "You are Authorised"});
-  }
-  else
-  {
-      console.log("Failure");
-      res.status(401);
-      res.json({"message": "UnAuthorised"});
-  }
+        console.log("Success");
+        res.status(200);
+        res.json({"message": "You are Authorised"});
+    }
+    else
+    {
+        console.log("Failure");
+        res.status(401);
+        res.json({"message": "UnAuthorised"});
+    }
 
 });
-
 
 module.exports = router;
