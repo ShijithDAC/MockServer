7a51b0db0dd67fb07c547af3561b813374324213
ShijithDAC <shijith@digitalapicraft.com>
2018-04-11 11:57:18 +0530

    enabling eslint

--- app.js
+++ app.js
@@ -9,40 +9,41 @@ const index = require('./routes/index');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+// View engine setup
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','html');
 app.set('layout','layout');
-app.engine('html', require('hogan-express'));
-app.locals.delimiters = '<% %>'
+app.engine('html',require('hogan-express'));
+app.locals.delimiters = '<% %>';
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Uncomment after placing your favicon in /public
+//App.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({"extended": false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/', index);
+app.use('/',index);
 
 
-// catch 404 and forward to error handler
-app.use((req, res, next)=> {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Catch 404 and forward to error handler
+app.use((req,res,next)=> {
+    const err = new Error('Not Found');
+
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use((err, req, res, next)=> {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Error handler
+app.use((err,req,res)=> {
+    // Set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // Render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
